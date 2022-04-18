34e1d145e86d30e082a624ac391d71f1c44ec643
Spike Vinz <spikevinz@gmail.com>
2022-04-18 15:29:01 +0800

    fixes search and instock filter adds utilty methods for search and instock

--- src/App.js
+++ src/App.js
@@ -1,114 +1,104 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
-import SearchBar from './components/SearchBar'
-import ProductCategory from './components/ProductCategory'
-import ProductRow from './components/ProductRow'
+import SearchBar from "./components/SearchBar";
+import ProductCategory from "./components/ProductCategory";
+import ProductRow from "./components/ProductRow";
+import { inStockItems, searchedItems } from "./helper/utils";
 
 const DATA_FROM_API = [
   {
-    category: 'Sporting Goods',
-    price: '$49.99',
+    category: "Sporting Goods",
+    price: "$49.99",
     stocked: true,
-    name: 'Football'
+    name: "Football",
   },
   {
-    category: 'Sporting Goods',
-    price: '$9.99',
+    category: "Sporting Goods",
+    price: "$9.99",
     stocked: true,
-    name: 'Baseball'
+    name: "Baseball",
   },
   {
-    category: 'Sporting Goods',
-    price: '$29.99',
+    category: "Sporting Goods",
+    price: "$29.99",
     stocked: false,
-    name: 'Basketball'
+    name: "Basketball",
   },
   {
-    category: 'Electronics',
-    price: '$99.99',
+    category: "Electronics",
+    price: "$99.99",
     stocked: true,
-    name: 'iPod Touch'
+    name: "iPod Touch",
   },
   {
-    category: 'Electronics',
-    price: '$399.99',
+    category: "Electronics",
+    price: "$399.99",
     stocked: false,
-    name: 'iPhone 5'
+    name: "iPhone 5",
   },
-  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
-]
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
+];
 
 const transformedData = DATA_FROM_API.reduce((prev, curr, index) => {
   if (index === 1) {
-    return [prev.category, prev, curr]
+    return [prev.category, prev, curr];
   }
 
   if (!!prev[index].category && prev[index].category !== curr.category) {
-    return [...prev, curr.category, curr]
+    return [...prev, curr.category, curr];
   }
 
-  return [...prev, curr]
-})
+  return [...prev, curr];
+});
 
 function App() {
-  const [items, setItems] = useState([])
-  const [inStock, setInStock] = useState(false)
-  const [keyword, setKeyword] = useState('')
+  const [items, setItems] = useState([]);
+  const [inStock, setInStock] = useState(false);
+  const [keyword, setKeyword] = useState("");
 
   const handleInStockStatusChange = (value) => {
-    setInStock(value)
-  }
+    setInStock(value);
+  };
 
   const handleSearchSubmit = (value) => {
-    setKeyword(value)
-  }
+    setKeyword(value);
+  };
 
   useEffect(() => {
-    setItems(transformedData)
-  }, [])
+    setItems(transformedData);
+  }, []);
 
   useEffect(() => {
-    if (inStock) {
-      const filteredItems = transformedData.filter(
-        (item) => item.stocked || typeof item === 'string'
-      )
-      setItems(filteredItems)
+    if (inStock && keyword) {
+      const itemsInStock = inStockItems(transformedData);
+      setItems(searchedItems(itemsInStock, keyword));
+    } else if (keyword) {
+      setItems(searchedItems(transformedData, keyword));
+    } else if (inStock) {
+      setItems(inStockItems(transformedData, keyword));
     } else {
-      setItems(transformedData)
+      setItems(transformedData);
     }
-  }, [inStock])
-
-  useEffect(() => {
-    if (keyword) {
-      const filteredItems = transformedData.filter((item) => {
-        return (
-          item?.name?.toLowerCase().includes(keyword.toLowerCase()) || typeof item === 'string'
-        )
-      })
-      setItems(filteredItems)
-    } else {
-      setItems(transformedData)
-    }
-  }, [keyword])
+  }, [keyword, inStock]);
 
   return (
-    <div className='App'>
+    <div className="App">
       <SearchBar
         inStock={inStock}
         onToggle={handleInStockStatusChange}
         onSearch={handleSearchSubmit}
       />
-      <div className='product-table'>
+      <div className="product-table">
         {items.map((item) => {
-          return typeof item === 'string' ? (
+          return typeof item === "string" ? (
             <ProductCategory key={item}>{item}</ProductCategory>
           ) : (
             <ProductRow key={item.name} item={item} />
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
